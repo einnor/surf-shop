b5514e11f876dc7374022180f404d8a41249d9cb
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-04 22:01:53 +0300

    Add user email and profile image

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,12 @@
 const User = require('../models/User');
 
 const postRegister = (req, res, next) => {
-  User.register(new User({ username: req.body.username }), req.body.password, (err) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    image: req.body.image
+  });
+  User.register(newUser, req.body.password, (err) => {
     if (err) {
       return next(err);
     }
