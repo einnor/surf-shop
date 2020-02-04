efd876b15124ce70b003b388cf43a35893383b75
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-04 14:42:13 +0300

    Declared all the REST routes for user authentication and profile

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const postsRouter = require('./routes/posts');
 const reviewsRouter = require('./routes/reviews');
-const usersRouter = require('./routes/users');
 
 const app = express();
 
@@ -24,7 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/posts', postsRouter);
 app.use('/posts/:id/reviews', reviewsRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,57 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
 });
 
+// USERS
+
+/* GET /register */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* GET /login */
+router.get('/login', (req, res, next) => {
+  res.send('GET /login');
+});
+
+/* POST /login */
+router.post('/login', (req, res, next) => {
+  res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+/* PUT /profile */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
+});
+
+/* GET /forgot-pw */
+router.get('/forgot-pw', (req, res, next) => {
+  res.send('GET /forgot-pw');
+});
+
+/* PUT /forgot-pw */
+router.post('/forgot-pw', (req, res, next) => {
+  res.send('PUT /forgot-pw');
+});
+
+/* GET /reset-pw/:token */
+router.get('/reset-pw/:token', (req, res, next) => {
+  res.send('GET /reset-pw/:token');
+});
+
+/* PUT /reset-pw/:token */
+router.put('/reset-pw/:token', (req, res, next) => {
+  res.send('GET /reset-pw/:token');
+});
+
+
 module.exports = router;
