4b8c446b87b056a33d854ec22323f6c990913625
Ronnie Nyaga <ronnienyaga@gmail.com>
2020-02-05 13:16:07 +0300

    Refactor /register post controller

--- controllers/index.js
+++ controllers/index.js
@@ -1,20 +1,13 @@
 const User = require('../models/User');
 
-const postRegister = (req, res, next) => {
+const postRegister = async (req, res, next) => {
   const newUser = new User({
     username: req.body.username,
     email: req.body.email,
     image: req.body.image
   });
-  User.register(newUser, req.body.password, (err) => {
-    if (err) {
-      return next(err);
-    }
-
-    console.log('user registered!');
-
-    res.redirect('/');
-  })
+  await User.register(newUser, req.body.password);
+  res.redirect('/');
 };
 
 module.exports = {
